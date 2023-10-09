a0ee18a60c56349235e02533b7b05a4828205f03
Jagannath Bhattaru Goswami <146979113+JagannathGoswami@users.noreply.github.com>
2023-10-09 17:57:44 +0530

    Add files via upload

---
+++ IprintInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTestProject
+{
+   public interface IprintInterface
+    {
+        string PrintMessage(int startNumber, int endNumber);
+
+        string ReverseString(string inputString);
+    }
+}
---
+++ PrintInterface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTestProject
+{
+    public class PrintNumber : IprintInterface
+    {
+        public string PrintMessage(int startNumber, int endNumber)
+        {
+            string PrintMessage = string.Empty;
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    PrintMessage += (i + " is fizzbuzz;");
+                }
+                else if (i % 3 == 0)
+                {
+                    PrintMessage += (i + " is fizz;");
+                }
+                else if (i % 5 == 0)
+                {
+                    PrintMessage += (i + " is buzz;");
+                }
+
+
+
+            }
+            return PrintMessage;
+        }
+
+        public string ReverseString(string inputString)
+        {
+            string result = string.Empty;
+           var newresult = inputString.ToCharArray();
+            for(int i = newresult.Length-1; i>=0; i--)
+            {
+                result += newresult[i];
+            }
+
+            return result;
+
+        }
+    }
+}
